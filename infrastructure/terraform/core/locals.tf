d228f4d8425311b313a8fe62748c55877a3b50c9
Isaac Naylor <isaac.naylor@education.gov.uk>
2026-09-09 17:29:48 +0100

    ci: added data blocks + cidr maths

--- infrastructure/terraform/core/locals.tf
+++ infrastructure/terraform/core/locals.tf
@@ -1,3 +1,4 @@
 locals {
   location_prefix = substr(var.region, 0, 3)
+  subnet_ranges = cidrsubnets(data.azurerm_virtual_network.res-2.address_space[0], 1, 2, 3, 3)
 }
--- infrastructure/terraform/core/main.tf
+++ infrastructure/terraform/core/main.tf
@@ -1,46 +1,42 @@
-resource "azurerm_resource_group" "res-0" {
-  location = var.region
-  name     = "${var.subscription_prefix}${var.environment_prefix}rg-${local.location_prefix}-core"
-  tags = {
-    Environment        = "Dev"
-    Product            = "Childcare Platform"
-    "Service Offering" = "Childcare Platform"
-  }
+data "azurerm_subscription" "current" {}
+
+data "azurerm_resource_group" "res-0" {
+  name = "${var.subscription_prefix}${var.environment_prefix}rg-${local.location_prefix}-core"
 }
 
-resource "azurerm_network_security_group" "res-1" {
-  location            = var.region
-  name                = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-nsg-01"
-  resource_group_name = azurerm_resource_group.res-0.name
-  security_rule       = []
-  tags = {
-    Environment        = "Dev"
-    Product            = "Childcare Platform"
-    "Service Offering" = "Childcare Platform"
-  }
+data "azurerm_network_security_group" "res-1" {
+  name = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-nsg-01"
+  resource_group_name = data.azurerm_resource_group.res-0.name
 }
 
-resource "azurerm_virtual_network" "res-2" {
-  address_space                  = ["10.226.188.0/25"]
-  dns_servers                    = ["10.210.64.4", "10.210.64.5"]
-  location                       = var.region
-  name                           = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-vn-01"
-  private_endpoint_vnet_policies = "Disabled"
-  resource_group_name            = azurerm_resource_group.res-0.name
-  tags = {
-    Environment        = "Dev"
-    Product            = "Childcare Platform"
-    "Service Offering" = "Childcare Platform"
-  }
+data "azurerm_virtual_network" "res-2" {
+  name = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-vn-01"
+  resource_group_name = data.azurerm_resource_group.res-0.name
 }
+
+// starting address range
+// azurerm_virtual_network.res-2.address_space
+// 10.226.188.0/25
+
+// cidrsubnet
+// cidrsubnets
+// -> cidrsubnets(azurerm_virtual_network.res-2.address_space, 1, 2, 3, 3)
+// > ["starting.../26". "starting.../27", starting.../28, starting.../28]
+// subnet_spaces
+
+// subnet_spaces[0]
+// subnet_spaces[1]
+// subnet_spaces[2]
+// subnet_spaces[3]
 
 resource "azurerm_subnet" "res-3" {
-  address_prefixes                              = ["10.226.188.96/28"]
+  address_prefixes                              = [local.subnet_ranges[2]]
+    # "10.226.188.96/28"
   default_outbound_access_enabled               = false
   name                                          = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-snet-backend"
   private_endpoint_network_policies             = "Disabled"
   private_link_service_network_policies_enabled = true
-  resource_group_name                           = azurerm_resource_group.res-0.name
+  resource_group_name                           = data.azurerm_resource_group.res-0.name
   service_endpoint_policy_ids                   = []
   service_endpoints                             = []
   virtual_network_name                          = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-vn-01"
@@ -52,62 +48,65 @@ resource "azurerm_subnet" "res-3" {
     }
   }
   depends_on = [
-    azurerm_virtual_network.res-2,
+    data.azurerm_virtual_network.res-2,
   ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "res-4" {
-  network_security_group_id = azurerm_network_security_group.res-1.id
+  network_security_group_id = data.azurerm_network_security_group.res-1.id
   subnet_id                 = azurerm_subnet.res-3.id
 }
 
 resource "azurerm_subnet" "res-5" {
-  address_prefixes                              = ["10.226.188.0/26"]
+  address_prefixes                              = [local.subnet_ranges[0]]
+    # "10.226.188.0/26"
   default_outbound_access_enabled               = false
   name                                          = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-snet-containerApps"
   private_endpoint_network_policies             = "Disabled"
   private_link_service_network_policies_enabled = true
-  resource_group_name                           = azurerm_resource_group.res-0.name
+  resource_group_name                           = data.azurerm_resource_group.res-0.name
   service_endpoint_policy_ids                   = []
   service_endpoints                             = []
   virtual_network_name                          = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-vn-01"
   depends_on = [
-    azurerm_virtual_network.res-2,
+    data.azurerm_virtual_network.res-2,
   ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "res-6" {
-  network_security_group_id = azurerm_network_security_group.res-1.id
+  network_security_group_id = data.azurerm_network_security_group.res-1.id
   subnet_id                 = azurerm_subnet.res-5.id
 }
 
 resource "azurerm_subnet" "res-7" {
-  address_prefixes                              = ["10.226.188.64/27"]
+  address_prefixes                              = [local.subnet_ranges[1]]
+    # "10.226.188.64/27"
   default_outbound_access_enabled               = false
   name                                          = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-snet-frontend"
   private_endpoint_network_policies             = "Disabled"
   private_link_service_network_policies_enabled = true
-  resource_group_name                           = azurerm_resource_group.res-0.name
+  resource_group_name                           = data.azurerm_resource_group.res-0.name
   service_endpoint_policy_ids                   = []
   service_endpoints                             = []
   virtual_network_name                          = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-vn-01"
   depends_on = [
-    azurerm_virtual_network.res-2,
+    data.azurerm_virtual_network.res-2,
   ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "res-8" {
-  network_security_group_id = azurerm_network_security_group.res-1.id
+  network_security_group_id = data.azurerm_network_security_group.res-1.id
   subnet_id                 = azurerm_subnet.res-7.id
 }
 
 resource "azurerm_subnet" "res-9" {
-  address_prefixes                              = ["10.226.188.112/28"]
+  address_prefixes                              = [local.subnet_ranges[3]]
+    # "10.226.188.112/28"
   default_outbound_access_enabled               = false
   name                                          = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-snet-githubActionsRunner"
   private_endpoint_network_policies             = "Disabled"
   private_link_service_network_policies_enabled = true
-  resource_group_name                           = azurerm_resource_group.res-0.name
+  resource_group_name                           = data.azurerm_resource_group.res-0.name
   service_endpoint_policy_ids                   = []
   service_endpoints                             = []
   virtual_network_name                          = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-vn-01"
@@ -119,12 +118,12 @@ resource "azurerm_subnet" "res-9" {
     }
   }
   depends_on = [
-    azurerm_virtual_network.res-2,
+    data.azurerm_virtual_network.res-2,
   ]
 }
 
 resource "azurerm_subnet_network_security_group_association" "res-10" {
-  network_security_group_id = azurerm_network_security_group.res-1.id
+  network_security_group_id = data.azurerm_network_security_group.res-1.id
   subnet_id                 = azurerm_subnet.res-9.id
 }
 
@@ -139,10 +138,10 @@ resource "azurerm_virtual_network_peering" "res-11" {
   peer_complete_virtual_networks_enabled = true
   remote_subnet_names                    = []
   remote_virtual_network_id              = var.remote_virtual_network_id
-  resource_group_name                    = azurerm_resource_group.res-0.name
+  resource_group_name                    = data.azurerm_resource_group.res-0.name
   use_remote_gateways                    = true
   virtual_network_name                   = "${var.subscription_prefix}${var.environment_prefix}-${local.location_prefix}-core-vn-01"
   depends_on = [
-    azurerm_virtual_network.res-2,
+    data.azurerm_virtual_network.res-2,
   ]
 }
