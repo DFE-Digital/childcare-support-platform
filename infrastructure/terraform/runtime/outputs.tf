5f583fd0581340d555692d102a49e7ae2293ed8e
Isaac Naylor <isaac.naylor@education.gov.uk>
2026-09-09 15:27:41 +0100

    fix: added required output from runtime module

--- infrastructure/terraform/runtime/outputs.tf
+++ infrastructure/terraform/runtime/outputs.tf
@@ -3,3 +3,9 @@ output "application_insights_instrumentation_key" {
   value       = azurerm_application_insights.res-19.instrumentation_key
   sensitive   = true
 }
+
+output "function_app_default_hostname" {
+  description = "Real, Azure-assigned default hostname of the runtime module's function app, used by APIM's backend."
+  value       = azurerm_function_app_flex_consumption.res-11.default_hostname
+  sensitive   = true
+}
