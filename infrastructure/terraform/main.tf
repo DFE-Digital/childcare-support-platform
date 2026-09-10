08f093e489248ff8e0207df388708505c04b39d4
Isaac Naylor <isaac.naylor@education.gov.uk>
2026-09-10 10:41:18 +0100

    fix: added azurerm backend

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "azurerm"
+      version = "5.4.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
 module "core" {
   source = "./core"
 
--- infrastructure/terraform/random.tf
+++ infrastructure/terraform/random.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-  }
-}
-
 resource "random_id" "unique_suffix" {
   byte_length = 2
 }
