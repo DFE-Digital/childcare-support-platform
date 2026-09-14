b4453de99abb48ce533a86e3cc2870f66832f2ea
Isaac Naylor <isaac.naylor@education.gov.uk>
2026-09-14 16:24:19 +0100

    feat: env var tag

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -8,6 +8,11 @@ variable "environment_prefix" {
   type        = string
 }
 
+variable "environment_tag" {
+  description = "Environment name for the required tag"
+  type        = string
+}
+
 variable "region" {
   description = "Azure region for resources that follow the primary deployment region (e.g. \"uksouth\"). Resources pinned to a different region (such as the ukwest API Management instance) are not parameterised by this variable."
   type        = string
