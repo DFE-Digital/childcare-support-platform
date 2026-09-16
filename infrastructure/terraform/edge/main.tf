08d1896cda5c34f3d084fc7b6ef213e6f1c3ce7d
Isaac Naylor <isaac.naylor@education.gov.uk>
2026-09-16 12:10:01 +0100

    fix: temporarily comment out rule

--- infrastructure/terraform/edge/main.tf
+++ infrastructure/terraform/edge/main.tf
@@ -162,6 +162,9 @@ resource "azurerm_cdn_frontdoor_origin" "res-53" {
     request_message        = "The request is from Front Door to the storage account for runtime data"
     target_type            = "blob"
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "azurerm_cdn_frontdoor_rule_set" "res-54" {
@@ -205,36 +208,36 @@ resource "azurerm_cdn_frontdoor_rule_set" "res-56" {
   name                     = "MapDataRequestToRuntimeContainer"
 }
 
-resource "azurerm_cdn_frontdoor_rule" "res-57" {
-  behaviour_on_match        = "Continue"
-  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.res-56.id
-  name                      = "MapDataRequestToRuntimeContainer"
-  order                     = 100
-  actions {
-    route_configuration_override {
-      caching {
-        behaviour           = "Disabled"
-        compression_enabled = false
-      }
-      origin_group {
-        cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.res-52.id
-        forwarding_protocol           = "MatchRequest"
-      }
-    }
-    url_rewrite {
-      destination_path                = "/${var.subscription_prefix}${var.environment_prefix}bc-${local.location_prefix}-source-data-01/app"
-      preserve_unmatched_path_enabled = true
-      source_pattern                  = "/data"
-    }
-  }
-  conditions {
-    request_path {
-      values     = ["/data/"]
-      operator   = "BeginsWith"
-      transforms = []
-    }
-  }
-}
+# resource "azurerm_cdn_frontdoor_rule" "res-57" {
+#   behaviour_on_match        = "Continue"
+#   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.res-56.id
+#   name                      = "MapDataRequestToRuntimeContainer"
+#   order                     = 100
+#   actions {
+#     route_configuration_override {
+#       caching {
+#         behaviour           = "Disabled"
+#         compression_enabled = false
+#       }
+#       origin_group {
+#         cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.res-52.id
+#         forwarding_protocol           = "MatchRequest"
+#       }
+#     }
+#     url_rewrite {
+#       destination_path                = "/${var.subscription_prefix}${var.environment_prefix}bc-${local.location_prefix}-source-data-01/app"
+#       preserve_unmatched_path_enabled = true
+#       source_pattern                  = "/data"
+#     }
+#   }
+#   conditions {
+#     request_path {
+#       values     = ["/data/"]
+#       operator   = "BeginsWith"
+#       transforms = []
+#     }
+#   }
+# }
 
 resource "azurerm_private_endpoint" "res-58" {
   custom_network_interface_name = "${var.subscription_prefix}${var.environment_prefix}nic-${local.location_prefix}-storage-endpoint-01"
