ce7e341bb7f08f95acf8d4a4ca1661719e855189
Isaac Naylor <isaac.naylor@education.gov.uk>
2026-09-21 15:44:19 +0100

    refactor: renamed tf resources in security

--- infrastructure/terraform/security/outputs.tf
+++ infrastructure/terraform/security/outputs.tf
@@ -1,19 +1,19 @@
 output "azf_identity_id" {
-  description = "ID of the function app user-assigned identity (azurerm_user_assigned_identity.res-3)."
-  value       = azurerm_user_assigned_identity.res-3.id
+  description = "ID of the function app user-assigned identity (azurerm_user_assigned_identity.azf-identity)."
+  value       = azurerm_user_assigned_identity.azf-identity.id
 }
 
 output "frontdoor_identity_id" {
-  description = "ID of the Front Door user-assigned identity (azurerm_user_assigned_identity.res-4)."
-  value       = azurerm_user_assigned_identity.res-4.id
+  description = "ID of the Front Door user-assigned identity (azurerm_user_assigned_identity.frontdoor-identity)."
+  value       = azurerm_user_assigned_identity.frontdoor-identity.id
 }
 
 output "keyvault_name" {
   description = "The name of the keyvault created by the Terraform"
-  value       = azurerm_key_vault.res-1.name
+  value       = azurerm_key_vault.key-vault.name
 }
 
 output "security_rg_name" {
   description = "The name of the security resource group"
-  value       = azurerm_resource_group.res-0.name
+  value       = azurerm_resource_group.security.name
 }
