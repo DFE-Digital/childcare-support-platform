f4938df335a2b188970c3fda13fdde4b16cf694f
charliedeeley <Charlie.DEELEY@education.gov.uk>
2026-09-24 10:11:43 +0100

    ci: versioning enabled on storage group with 30 day retention on saved versions

--- infrastructure/terraform/storage.tf
+++ infrastructure/terraform/storage.tf
@@ -47,12 +47,14 @@ resource "azurerm_storage_account" "site-data" {
     "Service Offering" = ""
   }
   blob_properties {
-    change_feed_enabled      = false
     last_access_time_enabled = false
-    versioning_enabled       = false
+    versioning_enabled = true
+    change_feed_enabled = true
+
     container_delete_retention_policy {
       days = 7
     }
+
     delete_retention_policy {
       days                     = 7
       permanent_delete_enabled = false
@@ -105,4 +107,27 @@ resource "azurerm_storage_account_static_website" "staticsite" {
   storage_account_id = azurerm_storage_account.site-data.id
   index_document     = "index.html"
   error_404_document = "index.html"
+}
+
+resource "azurerm_storage_management_policy" "site-data-lifecycle" {
+  storage_account_id = azurerm_storage_account.site-data.id
+
+  rule {
+    name    = "expire-old-blob-versions"
+    enabled = true
+
+    filters {
+      blob_types   = ["blockBlob"]
+      prefix_match = [
+        azurerm_storage_container.provider-container.name,
+        azurerm_storage_container.source-data-container.name,
+      ]
+    }
+
+    actions {
+      version {
+        delete_after_days_since_creation = 30
+      }
+    }
+  }
 }
